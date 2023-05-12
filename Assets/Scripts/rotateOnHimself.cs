b61ab85fc5bc7435716e3faeba7f0bd2deb05c25
fdiet <diet.florent1@gmail.com>
2023-05-12 18:05:17 +0200

    1.2 en cours

---
+++ Assets/Scripts/rotateOnHimself.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateOnHimself : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int x = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up,20.0f);
+        x++;
+        transform.localEulerAngles = new Vector3(x,0,0);
+        Debug.Log("yo");
+    }
+}
